3c690ef7d53324118634fe40f261bf7ab0158173
Liv Nelson <livnelson@me.com>
2022-10-11 22:22:42 -0700

    commit tree 42378f5b2be1bbcc92e29a004b10f7a85603a5f8

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+const headquarters = 42;
+
+function  distanceFromHqInBlocks(location) {
+    return Math.abs(location - headquarters)   
+}
+
+function distanceFromHqInFeet(location) {
+    return Math.abs(location - headquarters) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) < 400) {
+        let price = 0;
+        return price;
+    } else if (distanceTravelledInFeet(start, destination) < 2000) {
+        let price = ((distanceTravelledInFeet(start, destination) - 400) * .02);
+        return price;
+    } else if (distanceTravelledInFeet(start, destination) < 2500) {
+        let price = 25;
+        return price;
+    } else if (distanceTravelledInFeet(start, destination,) > 2500) {
+        return 'cannot travel that far';
+    }
+}
